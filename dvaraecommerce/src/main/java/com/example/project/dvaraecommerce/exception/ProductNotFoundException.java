1169b9201003431d2a9800c3642af21c138155f4
ahalya-k <145519967+ahalya-k@users.noreply.github.com>
2023-09-21 12:42:53 +0530

    Delete dvaraecommerce directory

--- dvaraecommerce/src/main/java/com/example/project/dvaraecommerce/exception/ProductNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.example.project.dvaraecommerce.exception;
-
-public class ProductNotFoundException extends RuntimeException {
-   
-    public ProductNotFoundException(String message) {
-        super(message);
-    }
-}
